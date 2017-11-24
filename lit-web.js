26ce6500380cb20cbc3c1d86bf28612ae438f138
Vijith Assar <vijithassar@yahoo.com>
2017-11-24 14:38:27 -0500

    execute literate scripts in order
    literate scripts should be loaded and executed predictably in the order they are declared on the page, instead of freely varying according to asynchronous network conditions

--- lit-web.js
+++ lit-web.js
@@ -43,18 +43,37 @@
     document.addEventListener('DOMContentLoaded', () => {
         // select all literate scripts in the DOM
         const literate_scripts = Array.from(document.querySelectorAll('script[type="text/literate-javascript"]'));
-        // process each literate script
-        literate_scripts.forEach(async function(script) {
-            const url = script.getAttribute('src');
-            if (url) {
-                // fetch script content
-                const markdown = await fetch(url).then(response => response.text());
+        // fetch the content for each literate script
+        const script_contents = literate_scripts
+            .map(script => {
+                const url = script.getAttribute('src');
+                if (url) {
+                    const promise = new Promise((resolve, reject) => {
+                        fetch(url)
+                            .then(response => {
+                                resolve(response.text());
+                            })
+                            .catch(error => {
+                                reject(error);
+                            });
+                    });
+                    return promise;
+                } else {
+                    return null;
+                }
+            });
+        // wait until all literate scripts have loaded
+        Promise.all(script_contents).then(results => {
+            // execute all literate scripts in order
+            results.forEach((markdown, index) => {
                 // compile script content
                 const code = compile(markdown);
+                // point sourcemap to Markdown document
+                const source_url = literate_scripts[index].src;
                 // execute code blocks
-                const wrapper = new Function(code + '//# sourceURL=' + script.src);
+                const wrapper = new Function(code + '//# sourceURL=' + source_url);
                 wrapper();
-            }
+            });
         });
     });
 })();
