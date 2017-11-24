ba9c5e543d5449dc0135e0acab9ca4c167f54e81
Vijith Assar <vijithassar@yahoo.com>
2017-11-24 14:45:01 -0500

    consistently refer to backticks as fences

--- lit-web.js
+++ lit-web.js
@@ -1,9 +1,9 @@
 (() => {
     'use strict';
     // test whether the line starts with backticks
-    const is_backticks = line => line.slice(0, 3) === '```';
+    const is_fence = line => line.slice(0, 3) === '```';
     // test whether the line starts with backticks followed by a javascript language specifier
-    const is_javascript_backticks = line => line.slice(0, 5) === '```js' || line.slice(0, 13) === '```javascript';
+    const is_javascript_fence = line => line.slice(0, 5) === '```js' || line.slice(0, 13) === '```javascript';
     // count backtick fences to make sure they are balanced
     const balanced_fences = code => code.split('```').length % 2 !== 0;
     // extract JavaScript code blocks from a Markdown string
@@ -15,20 +15,20 @@
         // split into lines
         const lines = markdown.split("\n");
         // count backticks
-        let fences = 0;
+        let fence_count = 0;
         // comment out Markdown
         const code = lines
             .map(line => {
-                const backticks = is_backticks(line);
-                const javascript_backticks = backticks ? is_javascript_backticks(line) : false;
+                const fence = is_fence(line);
+                const javascript_fence = fence ? is_javascript_fence(line) : false;
                 // increment the fence count if it's a valid
                 // opening or closing fence
-                if (javascript_backticks || (backticks && fences % 2 === 1)) {
-                    fences += 1;
+                if (javascript_fence || (fence && fence_count % 2 === 1)) {
+                    fence_count += 1;
                 }
                 // are we currently inside a code block
                 // or a Markdown documentation passage?
-                const is_markdown = fences % 2 === 0 || backticks;
+                const is_markdown = fence_count % 2 === 0 || fence;
                 if (is_markdown) {
                     return "// " + line;
                 } else {
